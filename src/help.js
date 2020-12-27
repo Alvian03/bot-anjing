dcf8df99c7fcde28956c7254d13d995196d0c858
Alvian03 <74562450+Alvian03@users.noreply.github.com>
2020-12-27 20:42:36 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,87 @@
+const help = (prefix) => { 
+	return `                 
+╔═ 〘 ♥BABUBOT♥ 〙══╗
+╚════════════════════╝ 
+
+╔ ═〘 OWNER COMMAND 〙═╗
+🌹 *${prefix}bc*
+🌹 *${prefix}block*
+🌹 *${prefix}unblock*
+╚ ═════════════╝
+╔ ═〘 STICKER COMMAND 〙╗
+🌹 *${prefix}sticker*
+🌹 *${prefix}tsticker*
+🌹 *${prefix}nulis*
+╚ ═════════════╝
+╔ ═〘 UPDATE COMMAND 〙╗
+🌹 *${prefix}text3d*
+🌹 *${prefix}ninjalogo*
+🌹 *${prefix}quotes*
+🌹 *${prefix}lirik*
+🌹 *${prefix}bucin*
+🌹 *${prefix}wolflogo*
+🌹 *${prefix}lionlogo*
+🌹 *${prefix}tebakgambar*
+🌹 *${prefix}caklontong*
+🌹 *${prefix}family100*
+🌹 *${prefix}game*
+🌹 *${prefix}textscreen <teks>*
+🌹 *${prefix}tahta <teks>*
+🌹 *${prefix}rtext <teks>*
+🌹 *${prefix}glitch <teks> | <teks>*
+🌹 *${prefix}party <teks>*
+🌹 *${prefix}lovemake <teks>*
+🌹 *${prefix}primbonjodoh <teks> | <teks>*
+🌹 *${prefix}artinama <nama>*
+🌹 *${prefix}ramalhp <nomor>*
+╚═════════════╝
+╔〘 COMMAND NSFW 〙╗
+🌹 *${prefix}loli*
+🌹 *${prefix}waifu*
+🌹 *${prefix}randomhentai*
+🌹 *${prefix}nsfwtrap*
+🌹 *${prefix}nsfwloli*
+🌹 *${prefix}nsfwneko*
+🌹 *${prefix}loli*
+╚═════════════╝
+╔═〘 COMMAND DOWNLOADER 〙╗
+🌹 *ytsearch* [search yt]
+🌹 *ytmp3* [link]
+🌹 *tiktok* [link]
+╚═════════════╝
+╔═〘 GROUB ONLY 〙═╗
+🌹 *${prefix}add* [62xxx]
+🌹 *${prefix}kick* [tag]
+🌹 *${prefix}setpp*
+🌹 *${prefix}promote* [tag]
+🌹 *${prefix}demote* [tag]
+🌹 *${prefix}group* [buka/tutup]
+🌹 *${prefix}welcome* [1/0]
+🌹 *${prefix}nsfw* [1/0]
+🌹 *${prefix}simih* [1/0]
+🌹 *${prefix}groupinfo*
+╚ ═════════════╝
+
+╔═〘 𝙊𝙒𝙉𝙀𝙍 〙═╗
+🌹 *${prefix}bc* 
+🌹 *${prefix}leave*
+🌹 *${prefix}clearall*
+🌹 *${prefix}setprefix*
+🌹 *${prefix}clone* [tag]
+🌹 *${prefix}block*
+🌹 *${prefix}unblock*
+🌹 *${prefix}getses*
+╚═════════════╝
+
+╔═〘 𝙊𝙏𝙃𝙀𝙍 〙═╗
+🌹 *${prefix}ytsearch*
+🌹 *${prefix}listadmin*
+🌹 *${prefix}blocklist*
+🌹 *${prefix}simi*
+🌹 *${prefix}wait*
+🌹 *${prefix}fitnah*
+🌹 *${prefix}tiktokstalk*
+🌹 *${prefix}url2img*
+╚═════════════╝`
+}
+exports.help = help
